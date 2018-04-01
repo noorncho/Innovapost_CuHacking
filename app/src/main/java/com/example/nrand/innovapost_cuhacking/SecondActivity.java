e30d8b94596ce8b899196713094e2d3f5ead4783
20Free <jon.austin@hotmail.ca>
2018-04-01 07:54:02 -0400

    Added EasyPost API.

---
+++ app/src/main/java/com/example/nrand/innovapost_cuhacking/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.nrand.innovapost_cuhacking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        ArrayList<String> providers = extras.getStringArrayList("PROVIDERS");
+        ArrayList<String> prices = extras.getStringArrayList("PRICES");
+        ArrayList<String> services = extras.getStringArrayList("SERVICES");
+        //TextView textView = findViewById(R.id.text);
+        //textView.setText(rateString);
+
+        ArrayList<DataModel> items = new ArrayList<>();
+
+        for(int i = 0; i < providers.size(); i++) {
+            items.add(new DataModel(providers.get(i), prices.get(i), services.get(i)));
+        }
+
+        ListView listView = findViewById(R.id.list_view);
+        CustomAdapter itemsAdapter = new CustomAdapter(this, items);
+        listView.setAdapter(itemsAdapter);
+    }
+}
