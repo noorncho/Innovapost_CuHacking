874e16c1fb614e4e3709d3ed0bcd7658a3fda2d9
20Free <jon.austin@hotmail.ca>
2018-04-01 07:43:22 -0400

    Delete EasyPost.java

--- app/src/main/java/com/example/nrand/innovapost_cuhacking/easyPost/EasyPost.java
+++
@@ -1,9 +0,0 @@
-package com.example.nrand.innovapost_cuhacking.easyPost;
-
-public abstract class EasyPost {
-  public static String API_BASE = "https://api.easypost.com/v2";
-  public static final String VERSION = "4.0.0";
-  public static String apiKey;
-  public static String apiVersion;
-  public static int readTimeout;
-}
